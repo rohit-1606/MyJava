798ef7acd110b756f92acad7d277b6e3ee88af50
rohit-1606 <rohitc1627@example.com>
2025-05-12 19:00:37 +0530

    first commit

---
+++ src/java_Bascics_2/Welcome.java
@@ -0,0 +1,9 @@
+package java_Bascics_2;
+
+public class Welcome {
+    public static void main(String[] args) {
+        System.out.println("welcome");
+       System.out.println();
+        System.out.println();
+    }
+}
---
+++ src/java_Basics/Helloworld.java
@@ -0,0 +1,12 @@
+package java_Basics;
+
+public class Helloworld {
+
+    public static void main(String[] args) {
+       System.out.println("hello world");
+       int i=1;
+       int j=2;
+       int c=i+j;
+       System.out.println(c);
+    }
+}
